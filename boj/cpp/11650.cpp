597345419704031737ae9ac63c1b5d06f669575d
subinan <53176366+subinan@users.noreply.github.com>
2020-07-22 00:33:51 +0900

    좌표 정렬하기

---
+++ boj/cpp/11650.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct cood {
+    int x, y;
+};
+
+cood arr[100001] = {0, };
+
+bool compare (cood a, cood b) {
+    return a.x != b.x ? a.x < b.x : a.y < b.y;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; ++i) scanf("%d %d", &arr[i].x, &arr[i].y);
+    
+    sort(arr,  arr + n, compare);
+    
+    for(int i = 0; i < n; ++i) printf("%d %d\n", arr[i].x, arr[i].y);\
+    
+    return 0;
+}
