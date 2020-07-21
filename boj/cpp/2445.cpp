4b8496881aa96d4d97c28c8e611501acb038c72d
subinan <53176366+subinan@users.noreply.github.com>
2020-07-22 00:26:48 +0900

    별 찍기 - 8

---
+++ boj/cpp/2445.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ 
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    for(int i=1;i<n;i++){
+		for(int j=0;j<i;j++)
+			printf("*");
+		for(int j=0;j<2*n-2*i;j++)
+			printf(" ");
+		for(int j=0;j<i;j++)
+			printf("*");
+        printf("\n");
+    }
+ 
+	for(int i=n;i>0;i--){
+		for(int j=0;j<i;j++)
+			printf("*");
+		for(int j=0;j<2*n-2*i;j++)
+			printf(" ");
+		for(int j=0;j<i;j++)
+			printf("*");
+        printf("\n");
+    }
+}
